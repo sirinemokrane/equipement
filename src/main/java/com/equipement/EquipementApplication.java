90f463b2183ba5138156aac1d4d4bb0526e58943
sirinemokrane <ms_mokrane@esi.dz>
2025-09-29 17:12:58 +0100

    Initial commit

---
+++ src/main/java/com/equipement/EquipementApplication.java
@@ -0,0 +1,13 @@
+package com.equipement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equipement/EquipementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
